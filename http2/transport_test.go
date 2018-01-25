503f43bdabc465a1092998ac17cff9ceb522478b
Antonin Amand <antonin.amand@gmail.com>
2018-01-25 23:45:59 +0100

    net/http2: add test to demonstrate transport can hang forever
    The transport can hang forever if the server hangs after accepting the
    connection and the request headers (or control frames) exceeds the connection
    write buffer. Without respect to the context's deadline.
    
    The ClientConn writes to the connection without setting a write deadline thus
    blocks forever if the buffer is flushed to the socket.
    
    go test -v -run TestTransportTimeoutServerHangs ./http2

--- http2/transport_test.go
+++ http2/transport_test.go
@@ -7,7 +7,9 @@ package http2
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"crypto/tls"
+	"encoding/hex"
 	"errors"
 	"flag"
 	"fmt"
@@ -788,6 +790,52 @@ func (r countingReader) Read(p []byte) (n int, err error) {
 	}
 	atomic.AddInt64(r.n, int64(len(p)))
 	return len(p), err
+}
+
+// TestTransportTimeoutServerHangs demonstrates that client can hang forever
+// when the server hangs and the headers exceed the conn buffer size (forcing a
+// flush). Without respect to the context's deadline.
+func TestTransportTimeoutServerHangs(t *testing.T) {
+	clientDone := make(chan struct{})
+	ct := newClientTester(t)
+	ct.client = func() error {
+		defer ct.cc.(*net.TCPConn).CloseWrite()
+		defer close(clientDone)
+
+		buf := make([]byte, 1<<19)
+		_, err := rand.Read(buf)
+		if err != nil {
+			t.Fatal("fail to gen random data")
+		}
+		headerVal := hex.EncodeToString(buf)
+
+		req, err := http.NewRequest("PUT", "https://dummy.tld/", nil)
+		if err != nil {
+			return err
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+		req = req.WithContext(ctx)
+		req.Header.Add("Authorization", headerVal)
+		_, err = ct.tr.RoundTrip(req)
+		if err == nil {
+			return errors.New("error should not be nil")
+		}
+		if ne, ok := err.(net.Error); !ok || !ne.Timeout() {
+			return fmt.Errorf("error should be a net error timeout was: +%v", err)
+		}
+		return nil
+	}
+	ct.server = func() error {
+		ct.greet()
+		select {
+		case <-time.After(5 * time.Second):
+		case <-clientDone:
+		}
+		return nil
+	}
+	ct.run()
 }
 
 func TestTransportReqBodyAfterResponse_200(t *testing.T) { testTransportReqBodyAfterResponse(t, 200) }
